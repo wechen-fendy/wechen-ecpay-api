7613f9818a756290d0dd93619cec7529734b89f1
weichen <wechen.tw@gmail.com>
2026-08-25 16:28:22 +0800

    Update ecpay.js

--- api/ecpay.js
+++ api/ecpay.js
@@ -24,13 +24,13 @@ module.exports = async (req, res) => {
   const ClientBackURL = 'https://wechen.tw';
 
   const tradeNo = `WEC${Date.now()}`;
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const d = new Date(new Date().getTime() + (8 * 60 * 60 * 1000));
+  const year = d.getUTCFullYear();
+  const month = String(d.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(d.getUTCDate()).padStart(2, '0');
+  const hours = String(d.getUTCHours()).padStart(2, '0');
+  const minutes = String(d.getUTCMinutes()).padStart(2, '0');
+  const seconds = String(d.getUTCSeconds()).padStart(2, '0');
   const date = `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
   
   const params = {
